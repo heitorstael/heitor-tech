b3cdc20d362795f76ce3d493b14050eec93b76bb
Heitor Stael <heitorstael@live.com>
2021-08-07 23:37:03 -0300

    starting the project

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1>Hello World!</h1>
+    <programmer-illustration width="150" height="150" />
+  </section>
+</template>
+
+<script>
+import ProgrammerIllustration from '~/components/home/ProgrammerIllustration'
+
+export default {
+  name: 'Home',
+  components: {
+    ProgrammerIllustration
+  }
+}
+</script>
