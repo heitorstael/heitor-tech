212613ab1d9435e90aa90b5c2ef39509d9168778
Heitor Stael <heitorstael@live.com>
2025-03-13 23:16:04 -0300

    feather icons + seo meta tags + beginning with aside

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,28 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/scss/global.scss'],
+
+  app: {
+    head: {
+      title: 'heitor.tech',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+      meta: [
+        { name: 'description', content: "Heitor Stael's personal website that contains his social media and portfolio." },
+        { name: "author", content: "Heitor Stael" },
+        {
+          name: "keywords",
+          content: "HTML, CSS, JavaScript, Web Development, Developer, NuxtJS, VueJS, Vue, Heitor, Stael, Full Stack, heitor tech, Laravel, portfolio",
+        },
+        { name: "theme-color", content: "#101010" },
+      ]
+    },
+  },
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -11,5 +33,7 @@ export default defineNuxtConfig({
         }
       }
     }
-  }
+  },
+
+  modules: ['nuxt-feather-icons']
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <section>
-    <h1>Home Page</h1>
-  </section>
+  <main>
+    <aside>
+      <h1>profile pic</h1>
+      <h1>name</h1>
+      <h1>bio</h1>
+      <span>follow me at</span>
+      <ul>
+        <li v-for="(item, i) in menuItems" :key="i">
+          <component :is="item.icon" size="2x"/>
+        </li>
+      </ul>
+    </aside>
+    <section></section>
+  </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @use "~/assets/scss/homepage.scss";
 </style>
+
+<script lang="ts" setup>
+  const menuItems = ref([
+    {
+      icon: resolveComponent('HomeIcon'),
+    },
+    {
+      icon: resolveComponent('UsersIcon'),
+    },
+    {
+      icon: resolveComponent('LayersIcon'),
+    },
+  ])
+</script>
